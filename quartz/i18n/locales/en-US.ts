e41159ae20291985c88686460cd45b48240a2784
elianamugar <emugar@bu.edu>
2026-05-04 22:52:45 -0700

    customize graph title

--- quartz/i18n/locales/en-US.ts
+++ quartz/i18n/locales/en-US.ts
@@ -39,7 +39,7 @@ export default {
       createdWith: "Created with",
     },
     graph: {
-      title: "Graph View",
+      title: "Graph View (zoom out to see the webs)",
     },
     recentNotes: {
       title: "Recent Notes",
