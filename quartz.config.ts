7f26e6d481501d7f946597ff827ce9878edb29ef
elianamugar <emugar@bu.edu>
2026-05-04 17:22:41 -0700

    fix quartz base url syntax

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "baseUrl: "elianamugar.github.io/Everything-Obsidian-Site",",
+    baseUrl: "elianamugar.github.io/Everything-Obsidian-Site",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
